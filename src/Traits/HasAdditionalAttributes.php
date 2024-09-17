97e7b631ba1a1597ad4de09a09b31f5d7ffa6f1b
renzoDC15 <renzo.carianga@gmail.com>
2024-09-17 11:58:19 +0800

    handle null on setter

--- src/Traits/HasAdditionalAttributes.php
+++ src/Traits/HasAdditionalAttributes.php
@@ -34,311 +34,310 @@ trait HasAdditionalAttributes
         $this->mergeFillable(['phase', 'block', 'lot', 'building', 'floor_area', 'lot_area']);
     }
 
-    public function setPhaseAttribute(string $value): self
+    public function setPhaseAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::PHASE_FIELD, $value);
-
         return $this;
     }
 
     public function getPhaseAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::PHASE_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::PHASE_FIELD) ?? '';
     }
 
-    public function setBlockAttribute(string $value): self
+    public function setBlockAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::BLOCK_FIELD, $value);
-
         return $this;
     }
 
     public function getBlockAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::BLOCK_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::BLOCK_FIELD) ?? '';
     }
 
-    public function setLotAttribute(string $value): self
+    public function setLotAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::LOT_FIELD, $value);
-
         return $this;
     }
 
     public function getLotAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::LOT_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::LOT_FIELD) ?? '';
     }
 
-    public function setBuildingAttribute(string $value): self
+    public function setBuildingAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::BUILDING_FIELD, $value);
-
         return $this;
     }
 
     public function getBuildingAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::BUILDING_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::BUILDING_FIELD) ?? '';
     }
 
-    public function setFloorAreaAttribute(float $value): self
+    public function setFloorAreaAttribute(?float $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::FLOOR_AREA_FIELD, $value);
-
         return $this;
     }
 
     public function getFloorAreaAttribute(): ?float
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::FLOOR_AREA_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::FLOOR_AREA_FIELD) ?? 0;
     }
 
-    public function setLotAreaAttribute(float $value): self
+    public function setLotAreaAttribute(?float $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::LOT_AREA_FIELD, $value);
-
         return $this;
     }
 
     public function getLotAreaAttribute(): ?float
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::LOT_AREA_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::LOT_AREA_FIELD) ?? 0;
     }
 
-    public function setUnitTypeAttribute(string $value): self
+    public function setUnitTypeAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::UNIT_TYPE_FIELD, $value);
-
         return $this;
     }
 
     public function getUnitTypeAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::UNIT_TYPE_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::UNIT_TYPE_FIELD) ?? '';
     }
 
-    public function setUnitTypeInteriorAttribute(string $value): self
+    public function setUnitTypeInteriorAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::UNIT_TYPE_INTERIOR_FIELD, $value);
-
         return $this;
     }
 
     public function getUnitTypeInteriorAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::UNIT_TYPE_INTERIOR_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::UNIT_TYPE_INTERIOR_FIELD) ?? '';
     }
 
-    public function setHouseColorAttribute(string $value): self
+    public function setHouseColorAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::HOUSE_COLOR_FIELD, $value);
-
         return $this;
     }
 
     public function getHouseColorAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::HOUSE_COLOR_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::HOUSE_COLOR_FIELD) ?? '';
     }
 
-    public function setRoofStyleAttribute(string $value): self
+    public function setRoofStyleAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::ROOF_STYLE_FIELD, $value);
-
         return $this;
     }
 
     public function getRoofStyleAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::ROOF_STYLE_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::ROOF_STYLE_FIELD) ?? '';
     }
 
-    public function setEndUnitAttribute(bool $value): self
+    public function setEndUnitAttribute(?bool $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::END_UNIT_FIELD, $value);
-
         return $this;
     }
 
     public function getEndUnitAttribute(): ?bool
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::END_UNIT_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::END_UNIT_FIELD) ?? false;
     }
-
-    public function setVerandaAttribute(bool $value): self
+    public function setVerandaAttribute(?bool $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::VERANDA_FIELD, $value);
-
         return $this;
     }
 
     public function getVerandaAttribute(): ?bool
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::VERANDA_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::VERANDA_FIELD) ?? false;
     }
 
-    public function setBalconyAttribute(bool $value): self
+    public function setBalconyAttribute(?bool $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::BALCONY_FIELD, $value);
-
         return $this;
     }
 
     public function getBalconyAttribute(): ?bool
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::BALCONY_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::BALCONY_FIELD) ?? false;
     }
 
-    public function setFirewallAttribute(bool $value): self
+    public function setFirewallAttribute(?bool $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::FIREWALL_FIELD, $value);
-
         return $this;
     }
 
     public function getFirewallAttribute(): ?bool
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::FIREWALL_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::FIREWALL_FIELD) ?? false;
     }
 
-    public function setEavesAttribute(bool $value): self
+    public function setEavesAttribute(?bool $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::EAVES_FIELD, $value);
-
         return $this;
     }
 
     public function getEavesAttribute(): ?bool
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::EAVES_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::EAVES_FIELD) ?? false;
     }
 
-    public function setBedroomsAttribute(int $value): self
+    public function setBedroomsAttribute(?int $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::BEDROOMS_FIELD, $value);
-
         return $this;
     }
 
     public function getBedroomsAttribute(): ?int
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::BEDROOMS_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::BEDROOMS_FIELD) ?? 0;
     }
 
-    public function setToiletsAndBathroomsAttribute(int $value): self
+    public function setToiletsAndBathroomsAttribute(?int $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::TOILETS_AND_BATHROOMS_FIELD, $value);
-
         return $this;
     }
 
     public function getToiletsAndBathroomsAttribute(): ?int
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::TOILETS_AND_BATHROOMS_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::TOILETS_AND_BATHROOMS_FIELD) ?? 0;
     }
 
-    public function setParkingSlotsAttribute(int $value): self
+    public function setParkingSlotsAttribute(?int $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::PARKING_SLOTS_FIELD, $value);
-
         return $this;
     }
 
     public function getParkingSlotsAttribute(): ?int
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::PARKING_SLOTS_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::PARKING_SLOTS_FIELD) ?? 0;
     }
 
-    public function setCarportsAttribute(int $value): self
+    public function setCarportsAttribute(?int $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::CARPORTS_FIELD, $value);
-
         return $this;
     }
 
     public function getCarportsAttribute(): ?int
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::CARPORTS_FIELD) ?? $default;
+        return $this->getAttribute('meta')->get(Property::CARPORTS_FIELD) ?? 0;
     }
 
-    public function setProjectCodeAttribute(string $value): self
+    public function setProjectCodeAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::PROJECT_CODE, $value);
-
         return $this;
     }
 
     public function getProjectCodeAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::PROJECT_CODE) ?? $default;
+        return $this->getAttribute('meta')->get(Property::PROJECT_CODE) ?? '';
     }
 
-    public function setProjectLocationAttribute(string $value): self
+    public function setProjectLocationAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::PROJECT_LOCATION, $value);
-
         return $this;
     }
 
     public function getProjectLocationAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::PROJECT_LOCATION) ?? $default;
+        return $this->getAttribute('meta')->get(Property::PROJECT_LOCATION) ?? '';
     }
 
-    public function setProjectAddressAttribute(string $value): self
+    public function setProjectAddressAttribute(?string $value): self
     {
+        if ($value === null) {
+            return $this;
+        }
         $this->getAttribute('meta')->set(Property::PROJECT_ADDRESS, $value);
-
         return $this;
     }
 
     public function getProjectAddressAttribute(): ?string
     {
-        $default = null;
-
-        return $this->getAttribute('meta')->get(Property::PROJECT_ADDRESS) ?? $default;
+        return $this->getAttribute('meta')->get(Property::PROJECT_ADDRESS) ?? '';
     }
 }
